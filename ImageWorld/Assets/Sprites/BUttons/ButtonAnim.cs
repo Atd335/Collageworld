0fb1d2af82920e17aa93f700db43deca939704f2
Unknown <atd335@nyu.edu>
2021-01-20 13:06:39 -0500

    added some button UIs

---
+++ ImageWorld/Assets/Sprites/BUttons/ButtonAnim.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnim : MonoBehaviour
+{
+    public Sprite[] buttonSprites;
+    SpriteRenderer SR;
+
+    MainMenuButtonScript button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = transform.parent.transform.parent.GetComponent<MainMenuButtonScript>();
+        SR = GetComponent<SpriteRenderer>();
+        currentFr = Random.Range(0,buttonSprites.Length);
+        animSpd = 10;
+    }
+
+    int frames;
+    int currentFr;
+
+    float animSpd;
+
+    private void Update()
+    {
+        if (!button) { return; }
+
+        if (button.isHovered)
+        {
+            transform.parent.GetComponent<SketchifyItem>().sketchSpd = 2;
+            animSpd = 4;
+        }
+        else
+        {
+            transform.parent.GetComponent<SketchifyItem>().sketchSpd = 20;
+            animSpd = 10;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        frames++;
+        if (frames%animSpd==0)
+        {
+            currentFr++;
+        }
+        if (currentFr==buttonSprites.Length)
+        {
+            currentFr = 0;
+        }
+
+        SR.sprite = buttonSprites[currentFr];
+
+    }
+}
